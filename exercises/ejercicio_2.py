03aa6c9c0583c9b1339575f05f959ad3a2c98a13
Santiago Cordoba <scordoba067@soyudemedellin.edu.co>
2023-02-10 07:55:46 -0500

    Implement exercise 2

--- exercises/ejercicio_2.py
+++ exercises/ejercicio_2.py
@@ -2,6 +2,10 @@ if __name__ == "__main__":
     import math
 
 
-    def area_circle(radius):
-        return math.pi * (radius ** 2)
+    def Circulo(R):
+        A = math.pi * R ** 2
+        return A
+    Radio = float(input('Ingrese radio para hallar su área: '))
+    A = (Circulo(Radio))
+    print(f'El área del circulo con radio {Radio} es igual a: {A:.3f}')
 
